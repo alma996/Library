3bdd409ed613185249790c66b534657f084dce4f
alma996 <almasadikovic96@gmail.com>
2020-01-24 15:58:50 +0100

    Initial commit of library project

--- server/migrations/20200120113755-add_user.js
+++ server/migrations/20200120113755-add_user.js
@@ -8,7 +8,7 @@ module.exports = {
       "last_name": "Sadiković",
       "email": "alma96@gmail.com",
       "password": "$2a$10$SwI9U6Vy2FJ4URVnE46mnujwRul0yGUeu9Sk4qt1TowzikLMCJ94O",
-      "created": "20.01.2020",
+      "created": "2020-11-11",
     }]
 
     await queryInterface.bulkInsert('Users', userData)
